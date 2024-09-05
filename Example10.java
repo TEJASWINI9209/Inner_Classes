8dae11bc7ea4459ec64f69e0b3129ee57d9d046e
TEJASWINI <annadatetejaswini@gmail.com>
2024-09-05 18:34:26 +0530

    Example on Inner class

---
+++ Example10.java
@@ -0,0 +1,25 @@
+class Demo
+{
+	void cyber(){
+		class Sample
+		{
+		}
+	}
+	void success(){
+		class Sample
+		{
+		}
+	}
+	void training(){
+		class Tech
+		{
+		}
+	}
+}
+class Example10 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
---
+++ Example9.java
@@ -0,0 +1,15 @@
+class Demo
+{
+	void test(){
+		class Sample
+		{
+		}
+	}
+}
+class Example9 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
